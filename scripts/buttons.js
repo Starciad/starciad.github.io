0545e2edc5289b04af908f20edaa1419accebd05
Starciad <davigamerlsf16@gmail.com>
2025-02-18 15:47:58 -0300

    feat: add pagination system.

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,5 +1,17 @@
+import { nextSection, previousSection } from "./pagination.js";
+
+// Add interactivity to link buttons
 document.querySelectorAll("#link-button").forEach(element => {
     element.addEventListener('click', function () {
         window.open(element.dataset.url, '_blank');
     });
+});
+
+// Add interactivity to the pagination system
+document.querySelector("#previous-section-pagination-button").addEventListener('click', function() {
+    previousSection();
+});
+
+document.querySelector("#next-section-pagination-button").addEventListener('click', function () {
+    nextSection();
 });
---
+++ scripts/pagination.js
@@ -0,0 +1,60 @@
+const hiddenClass = "hidden";
+
+const sectionContainer = document.querySelector(".app-section-container");
+const sections = document.querySelectorAll(".app-section");
+
+const numberOfSections = sections.length;
+
+let currentSectionIndex = 0;
+
+export function previousSection() {
+    let index = currentSectionIndex - 1;
+
+    if (index < 0) {
+        index = numberOfSections - 1;
+    }
+
+    setCurrentSection(index);
+}
+
+export function nextSection() {
+    let index = currentSectionIndex + 1;
+
+    if (index > numberOfSections - 1) {
+        index = 0;
+    }
+
+    setCurrentSection(index);
+}
+
+function setCurrentSection(index) {
+    hideAllSections();
+    showSection(index);
+
+    currentSectionIndex = index;
+}
+
+function showAllSections() {
+    for (let i = 0; i < numberOfSections; i++) {
+        showSection(i);
+    }
+}
+
+function hideAllSections() {
+    for (let i = 0; i < numberOfSections; i++) {
+        hideSection(i);
+    }
+}
+
+function showSection(index) {
+    sections[index].classList.remove(hiddenClass);
+}
+
+function hideSection(index) {
+    sections[index].classList.add(hiddenClass);
+}
+
+// ================================ //
+
+hideAllSections();
+showSection(currentSectionIndex);
